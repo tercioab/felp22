f90835d552ce655b7b950d7537f0041eb4b9f39b
walthercio Almeida <suaagenciainicial@gmail.com>
2022-06-12 12:25:42 -0300

    exclusão

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const box = document.getElementById("box-teste");
 box.addEventListener("click", function () {
 	if (box.className !== "mudar") {
 		box.className = "mudar";
-		box.innerHTML = `<h1> BRABÃO DE PEGAR ! </h1> `;
+		box.innerHTML = `<h1> FELP 22 ! </h1> `;
 	} else {
 		box.className = "box";
 		box.innerHTML = `<h1> QUEM DISSE QUE VOCÊ NÃO PODE VOAR MENOR ? </h1> `;
