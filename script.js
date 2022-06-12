23e331f8233098160cf763e40db0611665d9985c
walthercio Almeida <suaagenciainicial@gmail.com>
2022-06-12 03:40:07 -0300

    função nova no botao topo

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-const box = (document.getElementById("box-teste"));
+const box = document.getElementById("box-teste");
 
+const botao = document.getElementById("botao-topo");
 
+botao.addEventListener("click", function () {
+	if (box.className !== "mudar") {
+		box.className = "mudar";
+		box.innerHTML = `<h1> BRABÃO DE PEGAR ! </h1> `;
+	} else {
+		box.className = "box";
+		box.innerHTML = `<h1> QUEM DISSE QUE VOCÊ NÃO PODE VOAR MENOR ? </h1> `;
+	}
+});
 
-
-
-box.addEventListener('click', function() {
-    if (box.className !== "mudar") {
-        box.className = 'mudar'
-    } else {box.className = 'box'}
-})
-
-
+box.addEventListener("click", function () {
+	if (box.className !== "mudar") {
+		box.className = "mudar";
+		box.innerHTML = `<h1> BRABÃO DE PEGAR ! </h1> `;
+	} else {
+		box.className = "box";
+		box.innerHTML = `<h1> QUEM DISSE QUE VOCÊ NÃO PODE VOAR MENOR ? </h1> `;
+	}
+});
